094cd59c7e6921fb42a699bbec44785ad6541b67
hiago roberto <hiago@itecnologia.com.br>
2019-12-17 10:11:18 -0300

    Projeti criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+            p = new Posicao(3,4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadKey();
+        }
+    }
+}
